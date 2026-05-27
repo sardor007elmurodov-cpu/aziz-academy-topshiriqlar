65024901d7a37bf993706378c5937448981d5740
Elmurodov Sardor <sardor007elmurodov@gmail.com>
2026-05-27 13:23:28 +0500

    Add AI task: Bank hisobvaragining umumiy balansi

---
+++ ai_topshiriqlar/Bank_hisobvaragining_umumiy_balansi.py
@@ -0,0 +1,13 @@
+# Bank hisobvaragining umumiy balansi
+# Kurs: IT Dasturlash
+# Mavzu: type(), int(), float(), str() bilan casting
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+n = int(input())
+balance = 0
+for i in range(n):
+    operatsiya_miqdori = float(input())
+    balance += operatsiya_miqdori
+print(balance)
