0bd9affe728bf634478450c19dcece0f9498746e
Elmurodov Sardor <sardor007elmurodov@gmail.com>
2026-05-28 18:39:05 +0500

    Add: 13) Menyu + urinishlar soni [sardor007elmurodov-cpu]

---
+++ lms_kod_topshiriqlari/Kichik_menyu__1___qo_shish__2___ayirish_va_h_k_/13__Menyu___urinishlar_soni.py
@@ -0,0 +1,15 @@
+
+total = 0
+while True:
+    qator = input().split()
+    if len(qator) == 1:
+        break
+    a, b = map(int, qator)
+    n = int(input())
+    if n == 1:
+        
+        total = total + 1
+    elif n == 2:
+      
+        total = total + 1
+print(total)
