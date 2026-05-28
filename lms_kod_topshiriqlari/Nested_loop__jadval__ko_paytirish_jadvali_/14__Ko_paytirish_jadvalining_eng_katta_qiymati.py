ec1a10ef39272f02246965fec23e1beb3b3d48fb
Elmurodov Sardor <sardor007elmurodov@gmail.com>
2026-05-28 18:38:08 +0500

    Add: 14) Ko‘paytirish jadvalining eng katta qiymati [sardor007elmurodov-cpu]

---
+++ lms_kod_topshiriqlari/Nested_loop__jadval__ko_paytirish_jadvali_/14__Ko_paytirish_jadvalining_eng_katta_qiymati.py
@@ -0,0 +1,10 @@
+
+n, m = map(int, input().split())
+total = 0
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+            product = i * j
+            if product > total:
+                total = product
+             
+print(total)
