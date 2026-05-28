3b558164154e2ce0db61ec50fde89a7b335afd78
Elmurodov Sardor <sardor007elmurodov@gmail.com>
2026-05-28 11:01:08 +0500

    Add AI task: Foydalanuvchining yoshiga qarab kirish ruxsatini tekshirish

---
+++ ai_topshiriqlar/Foydalanuvchining_yoshiga_qarab_kirish_ruxsatini_tekshirish.py
@@ -0,0 +1,19 @@
+# Foydalanuvchining yoshiga qarab kirish ruxsatini tekshirish
+# Kurs: IT Dasturlash
+# Mavzu: Mantiqiy operatorlar: and, or, not
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+yosh = int(input())
+if yosh > 18 or yosh < 7:
+    if yosh > 18:
+        ortacha_ball = int(input())
+        if ortacha_ball > 80:
+            print("Kirish ruxsati berildi. Maxsus imtiyozlar berildi")
+        else:
+            print("Kirish ruxsati berildi")
+    else:
+        print("Kirish ruxsati berildi")
+else:
+    print("Kirish ruxsati berilmadi")
