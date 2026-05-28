c8ed0211d223cd40087941dc236b555acd42c16a
Elmurodov Sardor <sardor007elmurodov@gmail.com>
2026-05-28 10:54:13 +0500

    Add AI task: To'plamning eng katta elementi

---
+++ ai_topshiriqlar/Toplamning_eng_katta_elementi.py
@@ -0,0 +1,14 @@
+# To'plamning eng katta elementi
+# Kurs: IT Dasturlash
+# Mavzu: for sikli va range()
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+n = int(input())
+raqamlar = list(map(int, input().split()))
+eng_katta = raqamlar[0]
+for i in range(1, n):
+    if raqamlar[i] > eng_katta:
+        eng_katta = raqamlar[i]
+print(eng_katta)
